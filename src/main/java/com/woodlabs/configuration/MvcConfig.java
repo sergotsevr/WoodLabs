176cb309e9945d5e9bbf8cbc270ca05339997da6
sgotsev <sgotsev@cinimex.ru>
2019-12-03 09:58:05 +0300

    add security

---
+++ src/main/java/com/woodlabs/configuration/MvcConfig.java
@@ -0,0 +1,15 @@
+package com.woodlabs.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/main").setViewName("home");
+        registry.addViewController("/admin").setViewName("admin");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
