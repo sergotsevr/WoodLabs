9ff66ea7c95ad94831a4829113700007e0bac7bf
sgotsev <sgotsev@cinimex.ru>
2019-12-04 17:15:58 +0300

    working spring auth from db

--- src/main/java/com/woodlabs/configuration/WebSecurityConfig.java
+++ src/main/java/com/woodlabs/configuration/WebSecurityConfig.java
@@ -2,12 +2,17 @@ package com.woodlabs.configuration;
 
 import org.apache.tomcat.util.security.MD5Encoder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import javax.sql.DataSource;
 
@@ -30,13 +35,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .logout()
                     .permitAll();
     }
+  /*  @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("1")
+                        .password("1")
+                        .roles("USER")
+                        .build();
 
+        return new InMemoryUserDetailsManager(user);
+    }*/
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
                 .passwordEncoder(NoOpPasswordEncoder.getInstance())
                 .usersByUsernameQuery("select email, password, active from client where email=?")
-                .authoritiesByUsernameQuery("select u.email, ur.roles from client u inner join user_role ur on u.client_id = ur.client_id where u.email=?");
+                .authoritiesByUsernameQuery("select u.email, ur.roles from client u inner join user_role ur on u.client_id = ur.client_client_id where u.email=?");
     }
 }
