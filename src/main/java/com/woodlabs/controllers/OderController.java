70960b3752f5671144abcce8f3c866618f5931e0
sgotsev <sgotsev@cinimex.ru>
2019-12-09 17:08:09 +0300

    order update in progress

--- src/main/java/com/woodlabs/controllers/OderController.java
+++ src/main/java/com/woodlabs/controllers/OderController.java
@@ -2,6 +2,10 @@ package com.woodlabs.controllers;
 
 import com.woodlabs.dto.OrderDto;
 import com.woodlabs.entities.Order;
+import com.woodlabs.entities.enums.DeliveryMethod;
+import com.woodlabs.entities.enums.OrderStatus;
+import com.woodlabs.entities.enums.PaymentMethod;
+import com.woodlabs.entities.enums.PaymentStatus;
 import com.woodlabs.services.interfaces.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -27,6 +32,14 @@ public class OderController {
     public String update(Integer id, Model model){
         OrderDto orderDto = orderService.findById(id);
         model.addAttribute("order", orderDto);
+        List<PaymentMethod> paymentMethods = Arrays.asList(PaymentMethod.values());
+        model.addAttribute("paymentMethods", paymentMethods);
+        List<DeliveryMethod> deliveryMethods = Arrays.asList(DeliveryMethod.values());
+        model.addAttribute("deliveryMethods", deliveryMethods);
+        List<PaymentStatus> paymentStatuses = Arrays.asList(PaymentStatus.values());
+        model.addAttribute("paymentStatuses", paymentStatuses);
+        List<OrderStatus> orderStatuses = Arrays.asList(OrderStatus.values());
+        model.addAttribute("orderStatuses", orderStatuses);
         return "order/orderUpdate";
     }
 
