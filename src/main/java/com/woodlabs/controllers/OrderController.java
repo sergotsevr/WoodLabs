47cdf26651f9f08d22506219b229694e30845ccc
sgotsev <sgotsev@cinimex.ru>
2019-12-16 11:52:47 +0300

    format order controller

--- src/main/java/com/woodlabs/controllers/OrderController.java
+++ src/main/java/com/woodlabs/controllers/OrderController.java
@@ -7,28 +7,31 @@ import com.woodlabs.entities.enums.OrderStatus;
 import com.woodlabs.entities.enums.PaymentMethod;
 import com.woodlabs.entities.enums.PaymentStatus;
 import com.woodlabs.services.interfaces.OrderService;
-import com.woodlabs.utils.Util;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping(path = "/admin/order")
 public class OrderController {
     @Autowired
     OrderService orderService;
+
     @GetMapping
-    public String main(Model model){
+    public String main(Model model) {
         List<OrderDto> orderDtos = orderService.findAll();
         model.addAttribute("orders", orderDtos);
         return "order/orderMain";
     }
+
     @GetMapping("/update")
-    public String update(Integer id, Model model){
+    public String update(Integer id, Model model) {
         OrderDto orderDto = orderService.findById(id);
         model.addAttribute("order", orderDto);
         List<PaymentMethod> paymentMethods = Arrays.asList(PaymentMethod.values());
@@ -41,13 +44,15 @@ public class OrderController {
         model.addAttribute("orderStatuses", orderStatuses);
         return "order/orderUpdate";
     }
+
     @PostMapping("/update")
-    public String update(OrderDto orderDto, Model model){
+    public String update(OrderDto orderDto, Model model) {
         orderService.update(orderDto);
         return "redirect:";
     }
+
     @GetMapping("/create")
-    public String create(Model model){
+    public String create(Model model) {
         OrderDto orderDto = new OrderDto();
         model.addAttribute("order", orderDto);
         List<PaymentMethod> paymentMethods = Arrays.asList(PaymentMethod.values());
@@ -60,35 +65,34 @@ public class OrderController {
         model.addAttribute("orderStatuses", orderStatuses);
         return "order/orderCreate";
     }
+
     @PostMapping("/create")
-    public String create(OrderDto orderDto, Model model){
+    public String create(OrderDto orderDto, Model model) {
         orderService.add(orderDto);
         return "redirect:";
     }
+
     @GetMapping("/productUpdate")
-    public String productUpdate(Integer id, Model model){
+    public String productUpdate(Integer id, Model model) {
         OrderDto orderDto = orderService.findById(id);
-        Map<Product, Integer> products= orderDto.getGoodsList();
+        Map<Product, Integer> products = orderDto.getGoodsList();
         List<Product> productList = new ArrayList<>();
-        for (Map.Entry<Product, Integer>  entry : products.entrySet()) {
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
             productList.add(entry.getKey());
         }
         model.addAttribute("products", products);
         model.addAttribute("id", id);
         return "order/productInOrder";
     }
+
     @RequestMapping(value = "/productUpdate", method = RequestMethod.POST)
-    public String productUpdate(Model model, @RequestParam Map<String,String> allParams){
-        for (Map.Entry<String, String> product : allParams.entrySet()) {
-            if (Util.isNumeric(product.getKey())){
-                //product
-            }
-        }
+    public String productUpdate(Model model, @RequestParam Map<String, String> allParams) {
         Integer id = Integer.parseInt(allParams.get("id"));
-        return "redirect:productUpdate?id="+id;
+        return "redirect:productUpdate?id=" + id;
     }
+
     @GetMapping("/delete")
-    public String delete(Integer id, Model model){
+    public String delete(Integer id, Model model) {
         orderService.deleteById(id);
         return "redirect:";
     }
